708fb1cdc2d8f71ddcd0065be4b7ed49e4711bec
Cathy Tregaro <druar.cathy@gmail.com>
2024-09-11 13:02:57 +0200

    test avec functions.ipynb

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ import joblib
 from flask import Flask, jsonify, request
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
-from utils import cout_metier
-from utils_threshold import find_best_threshold
+from ipynb.fs.full.functions import cout_metier, find_best_threshold
 
 # Importer les données clients à prédire 
 data_client = pd.read_csv('subset_clients.csv')
